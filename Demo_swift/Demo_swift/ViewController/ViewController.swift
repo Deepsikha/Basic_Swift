999e8be9d34109812a49e4e7e9ff60670a347954
Deepsikha <ldipshikhal1986@gmail.com>
2017-01-27 17:40:42 +0530

    table view

--- Demo_swift/Demo_swift/ViewController/ViewController.swift
+++ Demo_swift/Demo_swift/ViewController/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
     @IBOutlet var Forward: UIButton!
+    @IBOutlet var tableControoler: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,11 +20,11 @@ class ViewController: UIViewController {
         self.navigationController?.navigationBar.isHidden = true
         
 //        Forward.frame = CGRectMake(100, 100, 50, 50)
-        
+        //MyTableView.frame = CGRectMake(20, 50, 250, 400)
     }
 
     @IBAction func ForwardAction(_ sender: UIButton) {
-        print("Button tapped")
+        print("Button tapped \u{1f44d}")
         let vc = SecondViewController(nibName:"secondView", bundle: nil)
         self.navigationController?.pushViewController(vc, animated: false)
     }
@@ -30,6 +33,14 @@ class ViewController: UIViewController {
         
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = "name"
+        return cell
+    }
+    
 }
 
