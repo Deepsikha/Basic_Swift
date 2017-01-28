ba0adb7792e2d0242806ec1263202f94bf2d3f85
Deepsikha <ldipshikhal1986@gmail.com>
2017-01-28 12:44:47 +0530

    no message

--- Demo_swift/Demo_swift/ViewController/ViewController.swift
+++ Demo_swift/Demo_swift/ViewController/ViewController.swift
@@ -12,20 +12,28 @@ class ViewController: UIViewController {
     
     
     @IBOutlet var Forward: UIButton!
-
+    @IBOutlet var label: UILabel!
+    @IBOutlet var email: UITextField!
+    @IBOutlet var pwd: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "FirstVC"
         self.navigationController?.navigationBar.isHidden = true
-        
+        label.font = label.font.withSize(20)
 //        Forward.frame = CGRectMake(100, 100, 50, 50)
         //MyTableView.frame = CGRectMake(20, 50, 250, 400)
     }
 
     @IBAction func ForwardAction(_ sender: UIButton) {
         print("Button tapped \u{1f44d}")
+        let pw = pwd.text
+        if pw == "makein" {
         let vc = SecondViewController(nibName:"secondView", bundle: nil)
         self.navigationController?.pushViewController(vc, animated: false)
+        }else{
+            let alt = UIAlertController(title: "", message: "Wrong Password", preferredStyle: UIAlertControllerStyle.alert)
+        }
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
