ad3e37827c407eddc3189dd1a28931f504db606e
Deepsikha <ldipshikhal1986@gmail.com Primary Public ldipshikhal1986@gmail.com Primary Public ldipshikhal1986@gmail.com>
2017-02-02 16:05:57 +0530

    no message

---
+++ Demo_swift/Demo_swift/custom cell/Demo_swift-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+//#ifndef Demo_swift-Bridging-Header_h
+//#define Demo_swift-Bridging-Header_h
+
+#import <MGSwipeTableCell/MGSwipeTableCell.h>
+//#endif
